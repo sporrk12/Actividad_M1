1148dd7836134affd65622ba4daeeadc8a92833e
Emmanuel Granados <76198594+sporrk12@users.noreply.github.com>
2022-11-15 22:37:20 -0600

    Update ModelCleaner.py

--- ActividadM1/ActividadM1/ModelCleaner.py
+++ ActividadM1/ActividadM1/ModelCleaner.py
@@ -32,46 +32,37 @@ class AgentCleanerRobot(mesa.Agent):
 
     def step(self):
 
-        # checar si ya esta limpio todo para evitar dar más steps
+        # Revisión de limpieza
         if self.model.dirtyCellRatio() >= 0.99:
             self.model.final_time = time.time()
         else:
 
             self.move()
             self.clean()
-
-
-
-            # hay que ir trackeando la cantidad de movimientos para cada agente.
             self.amount_movements = self.amount_movements + 1
 
 
 
 class ModelCleaner(mesa.Model):
 
-    # M y N son dimensiones de la habitación
+   
     def __init__(self, total_agents, height, width, percent):
-
-        # poblamos atributos miembros de la clase padre mesa.Model
         self.num_agents = total_agents
-        # queremos que sea multigrid el espacio porque varios agentes pueden estar sobre una misma celda.
-        self.grid = mesa.space.MultiGrid(height, width, True) # el True es una celda toroidal (las celdas no te sacan)
+        self.grid = mesa.space.MultiGrid(height, width, True) 
         self.schedule = mesa.time.RandomActivation(self)
 
-        # calcular tiempo de ejecucion del programa
+        # cSe calcula el tiempo de ejecución
         self.initial_time = time.time()
         self.final_time = 0
 
         self.celdasSucias = math.ceil((width * height) * percent)
         self.celdasLimpias = math.ceil((width * height) * (1 - percent))
 
-        # SÍ DEBE SER UN PORCENTAJE. La matriz va a ser una estructura de datos aparte.
         self.dirty_matrix = [[False for _ in range(height)] for _ in range(width)]
 
-        # False -> celda NO sucia (limpia)
-        # True -> celda sucia.
+    
 
-        # colocar celdas sucias
+        # Se colocan las celdas sucias
         for i in range(self.celdasSucias):
 
             rand_x = random.randint(0, width-1)
@@ -81,28 +72,29 @@ class ModelCleaner(mesa.Model):
 
 
 
-        # crear agentes
+        # Creaación de agentes
         for i in range(total_agents):
-            # agregamos un id unico al agente y lo sumamos a nuestro modelo (self)
+            
+            # Sumamos el agente a nuestro modelo
             a = AgentCleanerRobot(i, self)
 
             self.schedule.add(a)
 
-            # colocamos a los agentes en la celda [0,0] de la matriz
+            # Se colocoa a los agentes en la celda incial
             self.grid.place_agent(a, (1, 1))
 
     def step(self):
-        # imprimimos para ir registrando en que tiempo vamos.
+        # Imprimir cada step
         self.schedule.step()
 
     def isDirty(self, new_position):
         x, y = new_position
 
-        # retornar si en esta posicion de la matriz está sucio.
+        # Regresar si en esta posición de la matriz está sucia.
         return self.dirty_matrix[x][y]
 
     def setDirty(self, new_position):
-        # establecemos a verdadero el valor de la matriz en la posición para decir que está limpio.
+        # Se determina si la celda esta limpia
         self.celdasSucias = self.celdasSucias - 1
         self.celdasLimpias = self.celdasLimpias + 1
 
@@ -114,12 +106,12 @@ class ModelCleaner(mesa.Model):
             self.final_time = time.time()  # tiempo de ejecución del programa
 
     def total_movements(self):
-        # imprimir la cantidad de movimientos de todos los agentes
+        # iCantidad de moviminetos de los agentes
         return [agent.amount_movements for agent in self.schedule.agents]
 
-    # nos da el porcentaje de celdas limpias
+    # Porcentaje de celdas limpias
     def dirtyCellRatio(self):
         return self.celdasLimpias / (self.celdasSucias + self.celdasLimpias)
 
     def program_execution_time(self):
-        print('tiempo de ejecucion', self.final_time - self.initial_time, 'segundos')
+        print('Tiempo de ejecución', self.final_time - self.initial_time, 'segundos')
